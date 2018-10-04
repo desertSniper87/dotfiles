63c3e506dc2e1df545701e1a220c3fb772152eed
desertsniper87 <torshobuet@gmail.com>
2018-10-04 21:38:26 +0600

    new file

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,5 +1,9 @@
+#!/bin/sh 
 cp .vimrc ~/.vimrc
 cp .Xdefaults ~/.Xdefaults 
 cp .config/i3/config  ~/.config/i3/config
 cp -r scripts ~/scripts
-
+cp .xprofile ~/.xprofile
+cp .zshrc ~/.zshrc
+cp .config/qutebrowser/config.py ~/.config/qutebrowser/config.py
+#cp .config/qutebrowser/quickmar
---
+++ ubuntu_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd ~
+sudo apt install i3 i3lock i3blocks
