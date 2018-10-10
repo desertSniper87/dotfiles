ac35a25ebe76bdd15ea9a02b0145d45497d5d365
desertsniper87 <torshobuet@gmail.com>
2018-10-10 11:36:04 +0600

    copyq

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -5,7 +5,7 @@ sudo apt install git vim
 sudo apt install i3 i3lock i3blocks qutebrowser
 sudo apt install nodejs clang npm 
 sudo apt install lm_sensors redshift variety
-
+sudo apt install emacs copyq hg
 
 mkdir ~/.fonts
 git clone https://github.com/sunaku/tamzen-font
