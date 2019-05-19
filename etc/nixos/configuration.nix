64caff43f23f5f78dd00024ae5b12400ce670819
desertsniper87 <torshobuet@gmail.com>
2019-05-19 17:43:12 +0600

    new branch for laptop

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -21,7 +21,7 @@
   # Define on which hard drive you want to install Grub.
   # boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  networking.hostName = "nixos-desktop"; # Define your hostname.
+  networking.hostName = "nixos-laptop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
