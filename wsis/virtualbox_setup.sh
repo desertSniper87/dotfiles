fbcc1b8b8ddceb3c15b759714d65b25f7c32ed71
desertsniper87 <torshobuet@gmail.com>
2019-12-27 17:53:50 +0600

    vm script

---
+++ wsis/virtualbox_setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+sudo apt-get update && sudo apt-get upgrade
+
+# Access virtualbox shared folder
+sudo usermod -aG vboxsf torsho
+
+# Install chromium
+echo yes | sudo apt-get install chromium-browser
+
+# Install chrome
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update 
+echo yes | sudo apt-get install google-chrome-stable
+
+# Install brave
+
+sudo apt-get install curl
+
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+sudo sh -c 'echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com precise main" >> /etc/apt/sources.list.d/brave.list'
+
+sudo apt-get update
+echo yes | sudo apt-get install brave-browser brave-keyring
