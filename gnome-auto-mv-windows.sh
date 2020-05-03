92963b41880cfc6875092b02df72a9723b3e78b1
desertsniper87 <torshobuet@gmail.com>
2020-05-03 15:10:17 +0600

    ubuntu xdefaults

---
+++ gnome-auto-mv-windows.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Author            : desertsniper87 <torshobuet@gmail.com>
+# Date              : 13.03.2020
+# Last Modified Date: 13.03.2020
+
+
+gsettings set --schemadir=/home/torsho/.local/share/gnome-shell/extensions/auto-move-windows@gnome-shell-extensions.gcampax.github.com/schemas auto-move-windows@gnome-shell-extensions.gcampax.github.com application-list "[ 'thunderbird.desktop:2', 'shotwell.desktop:2', 'sublime.desktop:3', 'virtualbox.desktop:3, 'empathy.desktop:2']"
--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 
 cd ~
-# sudo apt install git vim-gnome
 # sudo apt install i3 i3lock i3blocks qutebrowser
-# sudo apt install nodejs clang npm 
+echo yes | sudo apt install git ctags
+echo yes | sudo apt install nodejs clang npm 
 echo yes | sudo apt install lm_sensors redshift variety
 echo yes | sudo apt install emacs copyq # hg
 echo yes | sudo apt install ack texstudio texlive-full htop aspell python3-pip nmap mysql-workbench mysql-server cmake apg python3-dev 
+
+curl -L git.io/antigen > antigen.zsh
+echo yes | sudo apt install zsh-theme-powerlevel9k zsh-autosuggestions zsh-history-substring-search autojump thefuck
+
+ # Install chrome
+
+#wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+#echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+#sudo apt-get update 
+#echo yes | sudo apt-get install google-chrome-stable
+
 
 # mkdir ~/.fonts
 # git clone https://github.com/sunaku/tamzen-font
