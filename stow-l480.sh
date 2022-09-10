e71a193be1033ec343f1664fecf4d381cc9cea6f
Samidhya Sarker <torshobuet@gmail.com>
2022-09-10 14:22:58 +0600

    qute

--- stow-l480.sh
+++ stow-l480.sh
@@ -3,4 +3,5 @@
 stow --verbose --restow neovim
 stow --verbose --restow ranger
 stow --verbose --restow vsnip
+stow --verbose --restow qutebrowser
 stow --verbose -d devices/l480 --target $HOME --restow bash
