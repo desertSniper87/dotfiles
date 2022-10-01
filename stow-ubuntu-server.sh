9419047e0db5cc159f89401a18c3c5b3ba919bc0
torsho <torshobuet@gmail.com>
2022-10-01 19:25:29 +0600

    ubuntu sever

---
+++ stow-ubuntu-server.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+DEVICE=ubuntu-server
+
+#stow --verbose --restow neovim
+#stow --verbose --restow ranger
+#stow --verbose --restow vsnip
+#stow --verbose --restow qutebrowser
+stow --verbose -d devices/$DEVICE --target "$HOME" --restow bash
+#stow --verbose -d devices/$DEVICE --target "$HOME" --restow alacritty
+stow --verbose --restow shell_scripts
