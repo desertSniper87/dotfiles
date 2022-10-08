dbe786dbf41af12c8202e424cb20cc066804cfec
desertsniper87 <torshobuet@gmail.com>
2022-10-08 06:16:30 +0000

    fix

---
+++ devices/ubuntu-server/vim/.vimrc
@@ -0,0 +1,15 @@
+set nocompatible
+syntax enable
+set cpoptions+=$
+set virtualedit=all
+set incsearch
+set hlsearch
+set ignorecase
+set smartcase
+set autochdir
+set clipboard=unnamed  
+set showcmd
+set backspace=indent,eol,start
+
+nnoremap <F9> :!%:p
+
--- stow-ubuntu-server.sh
+++ stow-ubuntu-server.sh
@@ -7,6 +7,7 @@ DEVICE=ubuntu-server
 #stow --verbose --restow vsnip
 #stow --verbose --restow qutebrowser
 stow --verbose -d devices/$DEVICE --target "$HOME" --restow bash
+stow --verbose -d devices/$DEVICE --target "$HOME" --restow vim
 #stow --verbose -d devices/$DEVICE --target "$HOME" --restow alacritty
 stow --verbose --restow shell_scripts
 stow --verbose --restow tmux
