2775c66788ce9fdc0518a9e05b3475b6d3dc79e8
desertsniper87 <torshobuet@gmail.com>
2022-11-16 22:19:17 +0600

    hard disk change

--- stow-l480.sh
+++ stow-l480.sh
@@ -9,3 +9,6 @@ stow --verbose --restow qutebrowser
 stow --verbose -d devices/$DEVICE --target "$HOME" --restow bash
 stow --verbose -d devices/$DEVICE --target "$HOME" --restow alacritty
 stow --verbose --restow shell_scripts
+stow --verbose --restow i3
+stow --verbose --restow Xdefaults
+stow --verbose --restow rxvt_perls
