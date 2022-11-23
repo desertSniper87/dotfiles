a591c2d9a244a2f9a7b1b834b13a23138e53b10b
desertsniper87 <torshobuet@gmail.com>
2022-11-23 22:05:03 +0600

    working

---
+++ stow-e203mah.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+DEVICE=l480
+
+stow --verbose --restow neovim
+stow --verbose --restow ranger
+stow --verbose --restow vsnip
+stow --verbose --restow qutebrowser
+stow --verbose -d devices/$DEVICE --target "$HOME" --restow bash
+stow --verbose -d devices/$DEVICE --target "$HOME" --restow alacritty
+stow --verbose --restow shell_scripts
+stow --verbose --restow i3
+stow --verbose --restow Xdefaults
+stow --verbose --restow rxvt_perls
